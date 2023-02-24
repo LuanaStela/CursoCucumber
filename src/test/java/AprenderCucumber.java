f056a8b695b04102ae3622bf6e4c87703125d349
Luana Stela <luana.arantes.lsa@gmail.com>
2023-02-23 21:20:48 -0300

    Atualização.

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,11 +1,13 @@
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class AprenderCucumber {
 
     @Given("que criei o arquivo corretamente")
     public void queCrieiOArquivoCorretamente() {
+        System.out.println("Qualquer coisa");
     }
     @When("executá-lo")
     public void executáLo() {
@@ -14,4 +16,21 @@ public class AprenderCucumber {
     public void aEspecificaçãoDeveFinalizarComSucesso() {
     }
 
+    private int contador = 0;
+
+    @Given("que o valor do contador é {int}")
+    public void queOValorDoContadorÉ(Integer int1) {
+        contador = int1;
+    }
+    @When("eu incrementar em {int}")
+    public void euIncrementarEm(Integer int1) {
+        contador = contador + int1;
+    }
+    @Then("o valor do contador será {int}")
+    public void oValorDoContadorSerá(Integer int1) {
+        System.out.println(int1);
+        System.out.println(contador);
+        System.out.println(int1 == contador);
+        Assert.assertTrue(int1 == contador);
+    }
 }
--- src/test/java/aprender_cucumber.feature
+++ src/test/java/aprender_cucumber.feature
@@ -7,3 +7,8 @@ Scenario: Deve executar especificação
   Given que criei o arquivo corretamente
   When executá-lo
   Then a especificação deve finalizar com sucesso
+
+Scenario: Deve incrementar contador
+  Given que o valor do contador é 15
+  When eu incrementar em 5
+  Then o valor do contador será 18
