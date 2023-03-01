903f1e48053a7eec64324bf7ab0068606e80e67c
Luana Stela <luana.arantes.lsa@gmail.com>
2023-03-01 18:34:34 -0300

    Atualização sobre o exercício.

--- src/test/java/steps/AlugarFilmeSteps.java
+++ src/test/java/steps/AlugarFilmeSteps.java
@@ -2,6 +2,9 @@ package steps;
 
 import entidades.Filme;
 import entidades.NotaAluguel;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -9,6 +12,7 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import servicos.AluguelService;
+import utils.DateUtils;
 
 import java.util.Calendar;
 
@@ -19,6 +23,7 @@ public class AlugarFilmeSteps {
     private AluguelService aluguel = new AluguelService();
     private NotaAluguel nota;
     private String erro;
+    private String tipoAluguel;
 
     @Given("^um filme com estoque de (\\d+) unidades$")
     public void umFilmeComEstoqueDeUnidades(Integer int1) {
@@ -34,7 +39,7 @@ public class AlugarFilmeSteps {
     @When("^alugar$")
     public void alugar() {
         try {
-            nota = aluguel.alugar(filme);
+            nota = aluguel.alugar(filme, tipoAluguel);
         } catch (RuntimeException e) {
             erro = e.getMessage();
         }
@@ -45,27 +50,32 @@ public class AlugarFilmeSteps {
        Assert.assertEquals(int1, nota.getPreco());
     }
 
-    @Then("^a data de entrega será no dia seguinte$")
-    public void aDataDeEntregaSeráNoDiaSeguinte() {
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH,1);
-
-/****/  Date dataRetorno = (Date) nota.getDataEntrega();
-        Calendar calRetorno = Calendar.getInstance();
-        calRetorno.setTime(dataRetorno);
-
-        Assert.assertEquals(cal.get(Calendar.DAY_OF_MONTH), calRetorno.get(Calendar.DAY_OF_MONTH));
-        Assert.assertEquals(cal.get(Calendar.MONTH), calRetorno.get(Calendar.MONTH));
-        Assert.assertEquals(cal.get(Calendar.YEAR), calRetorno.get(Calendar.YEAR));
-    }
-
-    @Then("^o estoque do filme será (\\d+) unidade$")
-    public void oEstoqueDoFilmeSeráUnidade(int int1) {
-    Assert.assertEquals(int1, filme.getEstoque());
+    @Then("^o estoque do filme será (\\d+) unidades$")
+    public void oEstoqueDoFilmeSeráUnidades(int int1) {
+        Assert.assertEquals(int1, filme.getEstoque());
     }
 
     @Then("^não será possível por falta de estoque$")
     public void nãoSeráPossívelPorFaltaDeEstoque() {
         Assertions.assertEquals("Filme sem estoque", erro);
     }
+
+    @Given("^que o tipo do aluguel seja (.*)$")
+    public void queOTipoDoAluguelSejaExtendido(String tipo) {
+        tipoAluguel = tipo;
+    }
+    @Then("^a data de entrega será em (\\d+) dias?$")
+    public void aDataDeEntregaSeráEmDias(int int1) {
+        Date dataEsperada = DateUtils.obterDataDiferencaDias(int1);
+/***/   Date dataReal = (Date) nota.getDataEntrega();
+
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+        Assert.assertEquals(format.format(dataEsperada), format.format(dataReal));
+    }
+    @Then("^a pontuação será de (\\d+) pontos$")
+    public void aPontuaçãoSeráDePontos(int int1) {
+        Assert.assertEquals(int1, nota.getPontuacao());
+    }
+
 }
--- src/test/resources/features/alugar_filme.feature
+++ src/test/resources/features/alugar_filme.feature
@@ -8,7 +8,7 @@ Scenario: Deve alugar um filme com sucesso
   And que o preço do aluguel seja R$ 3
   When alugar
   Then o preço do aluguel será R$ 3
-  And a data de entrega será no dia seguinte
+  And a data de entrega será em 1 dia
   And o estoque do filme será 1 unidade
 
 Scenario: Não deve alugar filme sem estoque
@@ -16,3 +16,21 @@ Scenario: Não deve alugar filme sem estoque
   When alugar
   Then não será possível por falta de estoque
   And o estoque do filme será 0 unidades
+
+Scenario: Deve dar condições especiais para categoria extendida
+  Given um filme com estoque de 2 unidades
+  And que o preço do aluguel seja R$ 4
+  And que o tipo do aluguel seja extendido
+  When alugar
+  Then o preço do aluguel será R$ 8
+  And a data de entrega será em 3 dias
+  And a pontuação será de 2 pontos
+
+Scenario: Deve alugar para categoria comum
+  Given um filme com estoque de 2 unidades
+  And que o preço do aluguel seja R$ 4
+  And que o tipo do aluguel seja comum
+  When alugar
+  Then o preço do aluguel será R$ 4
+  And a data de entrega será em 1 dia
+  And a pontuação será de 1 pontos
